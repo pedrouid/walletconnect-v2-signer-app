2688fa99cd6331c62d6481689d12a1a23b8fc40e
Pedro Gomes <pedrogomes94@gmail.com>
2021-02-18 20:00:08 +0100

    rn-nodeify

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+import "./shim.js";
+import { AppRegistry } from "react-native";
+import crypto from "crypto";
 
-import {AppRegistry} from 'react-native';
-import App from './src';
-import {name as appName} from './app.json';
+import App from "./src";
+import { name as appName } from "./app.json";
+
+console.log(crypto.randomBytes(32).toString("hex"));
 
 AppRegistry.registerComponent(appName, () => App);
